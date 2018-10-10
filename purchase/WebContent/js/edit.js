f10b4c9363fb7fbf2081c959f07595c55cbc1508
Administrator <Administrator@PC-201604301005>
2018-10-10 17:22:00 +0800

    订单包装数量问题修正

--- purchase/WebContent/js/edit.js
+++ purchase/WebContent/js/edit.js
@@ -228,7 +228,22 @@
 	         field : 'formatNum'  
 	     });
 	     
+	     if(formatNum.target.textbox('getValue') == ''){
+	    	 alert("null");
+	    	 return true;
+	     }
 	     var num = acount.target.textbox('getValue') / formatNum.target.textbox('getValue');
+	     var rest = acount.target.textbox('getValue') % formatNum.target.textbox('getValue');
+	     if(parseInt(acount.target.textbox('getValue')) < parseInt(formatNum.target.textbox('getValue'))){
+	    	 alert("为了方便发货，数量不得少于" +formatNum.target.textbox('getValue') ); 
+	    	acount.target.textbox('setValue' , '');
+	    	 return false ;
+	     }
+	     if(rest != 0 ){
+	    	   alert("为了方便发货，数量必须是" + formatNum.target.textbox('getValue') + "的整数倍");
+	    	  acount.target.textbox('setValue' , '');
+	    	   return false ;
+	     }
 	//     if(taxflag != '0'){
 	//    	    amount -= (amount*taxrate.target.textbox('getValue')*0.01);
 	//     }
